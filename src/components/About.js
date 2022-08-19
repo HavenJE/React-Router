d97bd767ad8973202c132c8ee04fd647392b1d2f
haven <haven.eldebaque@gmail.com>
2022-08-19 18:25:04 +1000

    SettingUP_Navbar

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='ui raised very padded text container segment'
+    style={{marginTop: '80px'}}
+    >
+        <h3 className='ui header'>About</h3>
+        <p>lorum epsim </p>
+    </div>
+  )
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div className='ui raised very padded text container segment'
+    style={{marginTop: '80px'}}
+    >
+        <h3 className='ui header'>Contact</h3>
+        <p>lorum epsim </p>
+    </div>
+  )
+}
